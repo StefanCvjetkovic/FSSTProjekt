67666e30a6b2067769fa316f4117444805152dff
456Xander <daumalex10200@gmail.com>
2017-01-09 21:37:03 +0100

    Created Test Classes and package and imported JUnitTest Library

---
+++ src/JumpAndRun/test/TestLineCollision.java
@@ -0,0 +1,22 @@
+package JumpAndRun.test;
+
+import static org.junit.Assert.*;
+
+import java.awt.Rectangle;
+import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
+
+import org.junit.Test;
+
+public class TestLineCollision {
+
+	@Test
+	public void test() {
+		Line2D.Double line = new Line2D.Double(0, 100, 100, 100);
+		Rectangle2D.Double rect = new Rectangle2D.Double(50, 75, 25, 50);
+		
+		boolean intersects = line.intersects(rect);
+		assertEquals(true, intersects);
+	}
+
+}
